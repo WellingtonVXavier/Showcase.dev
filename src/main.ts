253f06d3d6b20ecf63000960a27e1effc4067ff1
wellingtonvxavier <toomvxavier@outlook.com>
2025-07-16 15:41:11 -0300

    ajujste no primflex

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { importProvidersFrom } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)],
-}).catch(err => console.error(err));
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(BrowserAnimationsModule),
+  ],
+});
